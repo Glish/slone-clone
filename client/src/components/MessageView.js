f9fdf5ab2351a2037c5859156af9242c389527a5
Liam Manderson <liam.manderson@sfbagency.com>
2019-06-21 00:19:14 +0300

    Client Add: channel message view

--- client/src/components/MessageView.js
+++ client/src/components/MessageView.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
+import Moment from "react-moment";
 import { connect } from "react-redux";
-import ChannelModel from "../components/ChannelModel";
-// import { getChannels } from "../actions/channelActions";
+import ReactChatView from "react-chatview";
 import * as R from "ramda";
 
 const MessageView = props => {
@@ -12,15 +12,42 @@ const MessageView = props => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const MessageListing = () => {
-    return <div className="message-listing" />;
+  const MessageListing = messages => {
+    const calendarStrings = {
+      lastDay: "[Yesterday at] LT",
+      sameDay: "[Today at] LT",
+      sameElse: "LLLL"
+    };
+
+    return (
+      <ReactChatView reversed={true} flipped={true} className="message-listing">
+        <ul>
+          {R.map(
+            (message, idx) => (
+              <li>
+                <h3>
+                  <strong>{props.auth.user.nick} : </strong>
+                  <Moment calendar={calendarStrings}>
+                    {message.createdAt}
+                  </Moment>
+                </h3>
+                {message.message}
+              </li>
+            ),
+            props.messages
+          )}
+        </ul>
+      </ReactChatView>
+    );
   };
 
   const MessageField = () => <input className="message-field" type="text" />;
 
   return (
     <div className="message-view">
-      <MessageListing />
+      <MessageListing
+        messages={props.selectedChannel ? props.selectedChannel.messages : []}
+      />
       <MessageField />
     </div>
   );
@@ -28,7 +55,9 @@ const MessageView = props => {
 
 const mapStateToProps = state => ({
   auth: state.auth,
-  channel: state.channel
+  messages: state.channel.selectedChannel
+    ? state.channel.selectedChannel.Messages
+    : []
 });
 
 const mapDispatchToProps = {
